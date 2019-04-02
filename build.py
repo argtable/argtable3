04a84f6a34ff6541fac0a81f52bd608bab78c53b
Tom G. Huang <tomghuang@gmail.com>
2019-04-02 10:43:56 -0700

    fix: Travis CI failed to run Conan package manager

--- build.py
+++ build.py
@@ -5,8 +5,6 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="tomghuang", channel="testing")
-    builder.add(settings={"arch": "x86", "build_type": "Debug"},
-                options={}, env_vars={}, build_requires={})
-    builder.add(settings={"arch": "x86_64", "build_type": "Debug"},
+    builder.add(settings={"arch": "x86_64", "build_type": "Release"},
                 options={}, env_vars={}, build_requires={})
     builder.run()
