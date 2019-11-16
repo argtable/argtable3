83e637cbc92a8387a5a2e543d261e259bbd86c19
Tom G. Huang <tomghuang@gmail.com>
2019-11-15 21:03:18 -0800

    fix: Should reset parent->count after the for loop

--- src/arg_str.c
+++ src/arg_str.c
@@ -40,11 +40,12 @@
 
 static void arg_str_resetfn(struct arg_str* parent) {
     int i;
+    
     ARG_TRACE(("%s:resetfn(%p)\n", __FILE__, parent));
-    parent->count = 0;
     for (i = 0; i < parent->count; i++) {
         parent->sval[i] = "";
     }
+    parent->count = 0;
 }
 
 static int arg_str_scanfn(struct arg_str* parent, const char* argval) {
