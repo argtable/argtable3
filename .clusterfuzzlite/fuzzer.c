34f16eec8f8417d0961d7cf44b9659e5c4922b65
David Korczynski <david@adalogics.com>
2024-05-11 04:34:29 -0700

    Add ClusterFuzzLite integration
    Signed-off-by: David Korczynski <david@adalogics.com>

---
+++ .clusterfuzzlite/fuzzer.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "argtable3.h"
+#include "argtable3_private.h"
+#include "CuTest.h"
+#include "arg_getopt.h"
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    if (size < 3) {
+        return 0;
+    }
+
+    // Prepare input data
+    const char *input1 = (const char*)data;
+    const char *input2 = (const char*)(data + 1);
+    struct tm tm_value;
+
+    // Ensure null-terminated strings
+    char str1[size+1];
+    char str2[size];
+    memcpy(str1, input1, size);
+    str1[size] = '\0';
+    memcpy(str2, input2, size-1);
+    str2[size-1] = '\0';
+
+    // Call the target function
+    arg_strptime(str1, str2, &tm_value);
+
+    return 0;
+}
